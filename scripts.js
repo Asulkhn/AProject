380c260ab8123b5783ced6b8f5e4adea54fe8438
Asulkhn <andrishadu@gmail.com>
2024-09-11 14:23:01 +0500

    changing

--- scripts.js
+++ scripts.js
@@ -1,19 +1,4 @@
-jQuery(function () {
-    createSticky(jQuery("#sticky-wrap"));
-  });
-  
-  function createSticky(sticky) {
-    if (typeof sticky != "undefined") {
-      var pos = sticky.offset().top + 150,
-        win = jQuery(window);
-  
-      // scroll should really be debounced
-      win.on("scroll", function () {
-        if (win.scrollTop() > pos) {
-          sticky.addClass("stickyhead");
-        } else {
-          sticky.removeClass("stickyhead");
-        }
-      });
-    }
-  }
+window.addEventListener("scroll", function() {
+  let navbar = document.querySelector(".navbar");
+  navbar.classList.toggle("sticky", window.scrollY >0)
+})
